2bc0d5af311c87fe4e79b4d87849346625f5591c
marckevzz <marckevindelmundo@gmail.com>
2025-10-17 19:27:00 +0800

    implement tailadmin templates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { HelmetProvider } from "react-helmet-async";
+import { ThemeProvider } from "./tailadminsrc/context/ThemeContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </HelmetProvider>
   </StrictMode>
 );
